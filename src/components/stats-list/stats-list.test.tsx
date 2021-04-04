3bd91921166bbaa78989f76178ea23d9466c07b9
Daniel Morrissey <drmor9471@gmail.com>
2021-04-04 17:42:58 +1000

    Finish the stats list

--- src/components/stats-list/stats-list.test.tsx
+++ src/components/stats-list/stats-list.test.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
-import { act, create, ReactTestRenderer } from 'react-test-renderer'
-import { shallow, mount, render } from 'enzyme'
+import { mount } from 'enzyme'
 import StatsList from './stats-list'
 import { TimeState } from 'components/timer-display/timer-display'
 import { TimeLeft } from 'utilities/time.utils'
+import toJson from 'enzyme-to-json'
 
 describe('Stats List Tests', () => {
+    it('should display "NO TIMERS" when the history prop is empty', () => {
+        const component = mount(
+            <StatsList history={[]} deleteCallBack={() => {}} />
+        )
+        const lbl = component.find('.history__item--centered')
+        expect(lbl.text()).toBe('NO TIMERS')
+    })
+    it('should display "Total Time - None" when the history prop is empty', () => {
+        const component = mount(
+            <StatsList history={[]} deleteCallBack={() => {}} />
+        )
+        const lbl = component.find('.history__total')
+        expect(lbl.text()).toBe('Total Time - None')
+    })
+    it('should display the Total Time when the history prop is not empty', () => {
+        const historyItems = [
+            {
+                timeleft: {
+                    minutes: 15,
+                    seconds: 27,
+                    milliseconds: 75,
+                } as TimeLeft,
+                completionTime: new Date(2019, 1, 1, 7, 11, 0, 0),
+            } as TimeState,
+        ]
+        const component = mount(
+            <StatsList history={historyItems} deleteCallBack={() => {}} />
+        )
+        const lbl = component.find('.history__total')
+        expect(lbl.text()).toBe('Total Time - 15:27:75')
+    })
     it('should update the display list when the history prop changes', () => {
         const historyItems = [
             {
@@ -17,34 +48,11 @@ describe('Stats List Tests', () => {
                 completionTime: new Date(2019, 1, 1, 7, 11, 0, 0),
             } as TimeState,
         ]
-        let component: ReactTestRenderer = {} as ReactTestRenderer
-        act(() => {
-            component = create(
-                <StatsList history={[]} deleteCallBack={() => {}} />
-            )
-        })
-        expect(component.toJSON()).toMatchSnapshot()
-        act(() => {
-            component.update(
-                <StatsList history={historyItems} deleteCallBack={() => {}} />
-            )
-        })
-        expect(component.toJSON()).toMatchSnapshot()
+        const component = mount(
+            <StatsList history={[]} deleteCallBack={() => {}} />
+        )
+        expect(toJson(component)).toMatchSnapshot()
+        component.setProps({ history: historyItems })
+        expect(toJson(component)).toMatchSnapshot()
     })
-    // it('should toggle the button state from OFF to ON', () => {
-    //     let component: ReactTestRenderer = {} as ReactTestRenderer
-    //     act(() => {
-    //         component = create(<StatsList history={[]} />)
-    //     })
-    //     expect(component.toJSON()).toMatchSnapshot()
-    //     act(() => {
-    //         component.update(<StatsList history={[]} />)
-    //     })
-    //     expect(component.toJSON()).toMatchSnapshot()
-    // })
-    // it('should call a click handler when the toggle button is clicked', () => {
-    //     const component = shallow(<StatsList history={[]} />)
-    //     component.find('button').simulate('click')
-    //     expect([]).toBe('called!')
-    // })
 })
